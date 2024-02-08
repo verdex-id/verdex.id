7583089372e3800ad04250a7ecf139681041e838
SulTenZ <sultan.akmalghiffari@gmail.com>
2024-02-08 20:52:27 +0700

    component nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-8" style={{ backgroundColor: '#000017' }}>
+    <nav className="flex justify-between items-center p-8 bg-[#000017]">
       <div className="flex items-center mr-4">
-        <img src="/logo/verdex.svg" alt="Logo" className="h-8 w-auto ml-20" />
+        <Image src="/logo/verdex.svg" alt="Logo" width={40} height={40} className="ml-20" />
       </div>
       
       <ul className="flex space-x-6">
-        <li><a href="#" style={{ color: '#FFFFFF' }}>Home</a></li>
-        <li><a href="#" style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Course</a></li>
-        <li><a href="#" style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Our Team</a></li>
+        <li>
+          <Link href="#" className="text-white">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className="text-gray-500">
+            Course
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className="text-gray-500">
+            Our Team
+          </Link>
+        </li>
       </ul>
       
       <button className="flex items-center bg-white hover:bg-gray-200 text-black py-4 px-10 rounded-full mr-20">
         <span className="text-sm mr-2">Login</span>
-        <img src="/icons/passkey.svg" alt="Icon" className="h-6 w-auto inline" />
+        <Image src="/icons/passkey.svg" alt="Icon" width={24} height={24} />
       </button>
     </nav>
   );
