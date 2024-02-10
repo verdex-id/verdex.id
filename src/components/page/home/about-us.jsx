86cfb16cafbe209faedc8944dcef6872e82804ee
SulTenZ <sultan.akmalghiffari@gmail.com>
2024-02-10 11:01:41 +0700

    abt us & faq

--- src/components/page/home/about-us.jsx
+++ src/components/page/home/about-us.jsx
@@ -1,10 +1,31 @@
+import Image from "next/image";
 export default function HomeAboutUs() {
   return (
     <>
-      <div className="flex justify-center items-center min-h-dvh bg-blue-200">
-        <h1 className="font-bold text-xl text-center text-black">
-          This Is About Us Section
-        </h1>
+      <div className="flex justify-center items-center min-h-dvh bg-[#000017]">
+        <div className="flex-1 border-2 border-dashed border-transparent rounded-lg ml-60">
+          <h1 className="text-white text-7xl mb-8 text-left">
+            Verdex Lorem ipsum sit amet der Fringilla enim sut senectus arcuras scelerisque der sit aenean elit in der
+          </h1>
+
+          <p className="text-[#BEBEBE] text-left">
+            Lorem ipsum dolor sit amet consectetur. Iaculis varius porta purus pulvinar aliquam ut. Odio non neque egestas duis at aliquam. Et porttitor est ut orci. Eu euismod facilisis risus sed aliquet sagittis eu venenatis senectus.
+          </p>
+        </div>
+
+        <div className="flex flex-col justify-center border-2 border-dashed border-white border-opacity-25 rounded-lg p-10 w-1/3 mr-60 ml-40">
+          <div className="flex items-center justify-center mb-20 mt-10">
+            <Image
+              src="/logo/verdex.svg"
+              alt="Logo"
+              width={300}
+              height={300}
+            />
+          </div>
+          <p className="text-[#BEBEBE] text-left">
+            Lorem ipsum dolor sit amet consectetur. Iaculis varius porta purus<br></br> pulvinar aliquam ut. Odio non neque egestas duis at aliquam. Et<br></br> porttitor est ut orci. Eu euismod facilisis risus sed aliquet sagittis eu<br></br> venenatis senectus. Lorem ipsum dolor sit.
+          </p>
+        </div>
       </div>
     </>
   );
--- src/components/page/home/faq.jsx
+++ src/components/page/home/faq.jsx
@@ -1,10 +1,86 @@
 export default function HomeFAQ() {
   return (
     <>
-      <div className="flex justify-center items-center min-h-dvh bg-blue-200">
-        <h1 className="font-bold text-xl text-center text-black">
-          This Is FAQ Section
-        </h1>
+      <div className="flex justify-center items-center min-h-dvh bg-[#000017]">
+        <div className="text-white">
+          <div className="border-2 border-transparent border-opacity-25 border-2 rounded-3xl mb-4 w-[1500px] flex items-center justify-between">
+            <h1 className="text-6xl mb-8 text-left flex items-center">
+              <span>FAQ</span>
+              <img
+                src="/icons/help_center.svg"
+                alt="logo"
+                width={40}
+                height={40}
+                className="ml-2"
+              />
+            </h1>
+            <p className="mb-8 text-[#BEBEBE] text-left text-[12px]">
+              Sit amet spendisse quis feugiat morbi amet. Vitae nulla dignissim laoreet <br></br> amet bibendum feugiat. Massa amet sagittis diam mi. Dignissim vitae sed <br></br> eget ornare accumsan porttitor volutpat dignissim.
+            </p>
+          </div>
+
+          <div className="mb-8">
+            <div className="border-2 border-white border-opacity-25 border-2 rounded-3xl p-6 mt-4 mb-4 w-[1500px]">
+              <a href="" className="flex justify-between items-center text-2xl">
+                <span>What brings you to Verdex?</span>
+                <img
+                  src="/icons/arrow_outward.svg"
+                  alt="outwarded icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+            
+            <div className="border-2 border-white border-opacity-25 border-2 rounded-3xl p-6 mb-4 w-[1500px]">
+              <a href="" className="flex justify-between items-center text-2xl">
+                <span>What do you expect the most from the online learning experience?</span>
+                <img
+                  src="/icons/arrow_outward.svg"
+                  alt="outwarded icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+
+            <div className="border-2 border-white border-opacity-25 border-2 rounded-3xl p-6 mb-4 w-[1500px]">
+              <a href="" className="flex justify-between items-center text-2xl">
+                <span>How can Verdex help you achieve your learning goals?</span>
+                <img
+                  src="/icons/arrow_outward.svg"
+                  alt="outwarded icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+
+            <div className="border-2 border-white border-opacity-25 border-2 rounded-3xl p-6 mb-4 w-[1500px]">
+              <a href="" className="flex justify-between items-center text-2xl">
+                <span>Do you prefer learning independently or in a group?</span>
+                <img
+                  src="/icons/arrow_outward.svg"
+                  alt="outwarded icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+
+            <div className="border-2 border-white border-opacity-25 border-2 rounded-3xl p-6 mb-4 w-[1500px]">
+              <a href="" className="flex justify-between items-center text-2xl">
+                <span>Are there specific topics or skills you would like to develop?</span>
+                <img
+                  src="/icons/arrow_outward.svg"
+                  alt="outwarded icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
