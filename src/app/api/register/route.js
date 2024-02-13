ccd6ee616e077dd705346a01b3d4675292ef7c8f
Rizkia Adhy Syahputra <rizkia.as.actmp@gmail.com>
2024-02-13 12:16:16 +0700

    fix: api/register (medium-well code)

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,13 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+import { prismaErrorCode } from "@/lib/prisma";
+import { failResponse, successResponse } from "@/lib/response";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import Joi from "joi";
 
 const prisma = new PrismaClient();
 
-/**
- * Endpoint POST /api/register
- * create an user
- */
 export async function POST(request) {
+  const schema = Joi.object({
+    full_name: Joi.string()
+      .pattern(/^[A-Za-z\s']+$/)
+      .min(3)
+      .required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .min(6)
+      .required(),
+    email: Joi.string().email().required(),
+  });
+
   const req = await request.json();
+
+  const invalidReq = schema.validate(req);
+
+  if (invalidReq.error) {
+    return NextResponse.json(
+      ...failResponse("Invalid request format.", 403, invalidReq.error.details),
+    );
+  }
 
   // TODO : create hashing password function
 
@@ -19,18 +39,21 @@ export async function POST(request) {
     },
   };
 
-  const user = await prisma.user.create(arg);
+  let user;
 
-  console.log(user);
+  try {
+    user = await prisma.user.create(arg);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(...failResponse(prismaErrorCode[e.code], 500));
+    }
+    throw e;
+  }
 
   const res = {
     full_name: user.fullName,
     email: user.email,
   };
 
-  return Response.json({
-    status: 200,
-    data: res,
-    error: null,
-  });
+  return NextResponse.json(...successResponse(res));
 }
