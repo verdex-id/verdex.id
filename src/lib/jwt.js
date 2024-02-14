32dd1f744ac31c3a5e29b0892589dd0bb3166f57
Rizkia Adhy Syahputra <rizkia.as.actmp@gmail.com>
2024-02-14 10:00:51 +0700

    fix(jwt): JWEDecryptionFailed condition

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -2,19 +2,41 @@ import { nanoid } from "nanoid";
 import * as jose from "jose";
 
 const key = jose.base64url.decode(process.env.TOKEN_SYMMETRIC_KEY);
+
 /**
- * Creates a new encrypted token with the given user ID and duration.
- * @param {string} userId The ID of the user for whom the token is created.
- * @param {number} duration The duration of the token validity in minutes.
- * @returns {Promise<[string, object]>} A promise that resolves to an array containing the encrypted token and payload.
+ * Create a JWT token for the given user ID with specified duration.
+ * @param {string} userId - The user ID for whom the token is created.
+ * @param {number} duration - The duration of the token validity in hours.
+ * @returns {Promise<[string|null, Object|null, string|null]>} A Promise that resolves with an array containing:
+ * - The encrypted token string.
+ * - The token payload object.
+ * - An error message, if any.
+ * @example
+ * // Usage example:
+ * const [encryptedToken, payload, errorMessage] = await createToken("user123", 24);
+ * if (errorMessage) {
+ *   console.error("Error:", errorMessage);
+ * } else {
+ *   console.log("Encrypted Token:", encryptedToken);
+ *   console.log("Payload:", payload);
+ * }
  */
 export async function createToken(userId, duration) {
   let currentDate = new Date();
 
-  const expirationTime = new Date(currentDate.getTime() + duration * 60000);
+  const expirationTime = new Date(currentDate.getTime() + duration * 3600000);
+
+  const id = nanoid();
+  if (!id) {
+    return [
+      null,
+      null,
+      "Failed to generate random UUID. Please try again later.",
+    ];
+  }
 
   const payload = {
-    id: nanoid(),
+    id: id,
     userId: userId,
     createdAt: currentDate,
     expiredAt: expirationTime,
@@ -24,28 +46,37 @@ export async function createToken(userId, duration) {
     .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
     .encrypt(key);
 
-  return [encryptedToken, payload];
+  return [encryptedToken, payload, null];
 }
 
 /**
- * Verifies the validity of a token by decrypting and checking its payload.
- * @param {string} token The token to be verified.
- * @returns {Promise<{ isValid: boolean, payload: object }>} A promise that resolves to an object indicating whether the token is valid and its payload.
+ * Verify the validity of a JWT token.
+ * @param {string} token - The JWT token to be verified.
+ * @returns {Promise<[boolean, Object|null]>} A Promise that resolves with an array containing:
+ * - A boolean indicating whether the token is valid or not.
+ * - The token payload object if the token is valid, or null if the token is invalid.
+ * @example
+ * // Usage example:
+ * const [isValid, payload] = await verifyToken("your_jwt_token_here");
+ * if (isValid) {
+ *   console.log("Token is valid.");
+ *   console.log("Payload:", payload);
+ * } else {
+ *   console.log("Token is invalid or expired.");
+ * }
  */
 export async function verifyToken(token) {
-  const { payload, protectedHeader } = await jose.jwtDecrypt(token, key);
+  const payload = await wrapper(token);
 
-  if (isExpired(payload)) {
-    return {
-      isValid: false,
-      payload: payload,
-    };
+  if (!payload) {
+    return [false, null];
   }
 
-  return {
-    isValid: true,
-    payload: payload,
-  };
+  if (isExpired(payload)) {
+    return [false, payload];
+  }
+
+  return [true, payload];
 }
 
 function isExpired(payload) {
@@ -53,4 +84,15 @@ function isExpired(payload) {
   const expireTime = new Date(payload.expiredAt);
 
   return currentTime > expireTime;
+}
+
+async function wrapper(token) {
+  try {
+    const { payload, protectedHeader } = await jose.jwtDecrypt(token, key);
+    return payload;
+  } catch (e) {
+    if (e instanceof jose.errors.JWEDecryptionFailed) {
+      return null;
+    }
+  }
 }
