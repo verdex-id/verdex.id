ebe0754013b8b4eeafa41f7d1ede4ad76ae77168
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-02-18 14:12:47 +0700

    bcrypt and refactor lib/password.js

--- src/lib/password.js
+++ src/lib/password.js
@@ -1,23 +1,12 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export async function comparePassword(password, hashedPassword) {
-    const match = await bcrypt.compare(password, hashedPassword);
-    return match;
-  }
+  const match = await bcrypt.compare(password, hashedPassword);
+  return match;
+}
 
-export  async function hashPassword(password) {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-  }
-
-
-  
-
-
-
-
-// export function helloWorld (nama){
-// console.log (`helloWorld by  ${nama}`)
-// return  `Hello World from ${nama}`
-// }  
+export async function hashPassword(password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
