c00e8d81c0844f11a16fb07e2fed7f79a79c868c
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-07-30 20:26:58 +0900

    fix(prisma): refresh prisma schema

--- prisma/migrations/20240211052350_session_refresh_token/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Session` MODIFY `refreshToken` VARCHAR(255) NULL;
--- prisma/migrations/20240211055435_session_refresh_token_2/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Session_refreshToken_key` ON `Session`;
-
--- AlterTable
-ALTER TABLE `Session` MODIFY `refreshToken` TEXT NULL;
